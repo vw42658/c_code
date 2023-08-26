a3500d42d96a102dd5c8f10a718152518f5d9147
vw42658 <1433431037@qq.com>
2023-08-26 15:43:41 +0800

    变量与常量

---
+++ change.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include  <stdio.h>
+int main()
+{
+	//double weight = 75.3;
+	// 浮点型
+	//weight = weight - 10;
+	//printf("%lf\n", weight);
+	//int age = 20;
+	// 整型
+	//age = age + 1;
+	//printf("%d\n", age);
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum=%d\n", sum);
+	return 0;
+}
